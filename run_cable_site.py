c7d12c888c288cd14bcddf89ef0ec3b30d4942eb
mdekauwe <mdekauwe@gmail.com>
2017-09-23 21:06:35 +1000

    refactored site script

--- run_cable_site.py
+++ run_cable_site.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
 """
-Run CABLE either for a subset(single) flux set, or all the sites in a directory.
-This script also allows you to edit the namelist internal.
+Site run - just biophysics
+==========================
 
-Todo
-----
- * Pass site dictionary so that the namelist can vary by site.
+Run CABLE either for a single site, a subset, or all the flux sites pointed to
+in the met directory. The script creates cable.nml file.
 
 That's all folks.
 """
 __author__ = "Martin De Kauwe"
-__version__ = "1.0 (14.10.2017)"
+__version__ = "1.0 (14.09.2017)"
 __email__ = "mdekauwe@gmail.com"
 
 import os
@@ -22,52 +21,61 @@ import tempfile
 
 class RunCable(object):
 
-    def __init__(self, driver_dir, met_files, output_dir, nml_fn, veg_param_fn,
-                 log_dir, exe, aux_dir, verbose):
+    def __init__(self, met_files, output_dir, log_dir, aux_dir,
+                 nml_fn, veg_param_fn, soil_fn, grid_fn, cable_exe, verbose):
 
-        self.driver_dir = driver_dir
         self.met_files = met_files
         self.output_dir = output_dir
+        self.log_dir = log_dir
         self.nml_fn = nml_fn
         self.veg_param_fn = veg_param_fn
-        self.log_dir = log_dir
-        self.cable_exe = exe
+        self.grid_fn = grid_fn
         self.aux_dir = aux_dir
+        self.veg_dir = os.path.join(self.aux_dir, "core/biogeophys")
+        self.grid_dir = os.path.join(self.aux_dir, "offline")
+        self.cable_exe = cable_exe
         self.verbose = verbose
+        self.soil_fn = soil_fn
 
     def main(self):
 
         for fname in self.met_files:
             site = os.path.basename(fname).split(".")[0]
-
-            out_fname = os.path.join(self.output_dir, "%s_out.nc" % (site))
-            if os.path.isfile(out_fname):
-                os.remove(out_fname)
-
-            out_log_fname = os.path.join(self.log_dir, "%s_log.nc" % (site))
-            if os.path.isfile(out_log_fname):
-                os.remove(out_log_fname)
+            (out_fname, out_log_fname) = self.clean_up_old_files(site)
 
             replace_dict = {
                             "filename%met": "'%s'" % (fname),
                             "filename%out": "'%s'" % (out_fname),
                             "filename%log": "'%s'" % (out_log_fname),
                             "filename%restart_out": "' '",
-                            "filename%type": "'%sgridinfo_CSIRO_1x1.nc'" % (self.driver_dir),
-                            "filename%veg": "'%s%s'" % (self.driver_dir, veg_param_fn),
-                            "filename%soil": "'%sdef_soil_params.txt'" % (self.driver_dir),
+                            "filename%type": "'%s'" % (os.path.join(self.grid_dir, self.grid_fn)),
+                            "filename%veg": "'%s'" % (os.path.join(self.veg_dir, self.veg_param_fn)),
+                            "filename%soil": "'%s'" % (os.path.join(self.veg_dir, self.soil_fn)),
                             "output%restart": ".FALSE.",
                             "fixedCO2": "380.0",
                             "casafile%phen": "'%s'" % (os.path.join(self.aux_dir, "core/biogeochem/modis_phenology_csiro.txt")),
                             "casafile%cnpbiome": "'%s'" % (os.path.join(self.aux_dir, "core/biogeochem/pftlookup_csiro_v16_17tiles.csv")),
             }
             self.adjust_param_file(replace_dict)
+            self.run_me()
 
-            # run the model
-            if self.verbose:
-                os.system("%s" % (self.cable_exe))
-            else:
-                os.system("%s >& %s" % (self.cable_exe, out_log_fname))
+    def clean_up_old_files(self, site):
+        out_fname = os.path.join(self.output_dir, "%s_out.nc" % (site))
+        if os.path.isfile(out_fname):
+            os.remove(out_fname)
+
+        out_log_fname = os.path.join(self.log_dir, "%s_log.txt" % (site))
+        if os.path.isfile(out_log_fname):
+            os.remove(out_log_fname)
+
+        return (out_fname, out_log_fname)
+
+    def run_me(self):
+        # run the model
+        if self.verbose:
+            os.system("%s" % (self.cable_exe))
+        else:
+            os.system("%s 1>&2" % (self.cable_exe))
 
     def adjust_param_file(self, replacements):
         """ adjust model parameters in the file and save over the original.
@@ -117,25 +125,40 @@ class RunCable(object):
             elif not row.startswith("&"):
                 key = row.split("=")[0]
                 val = row.split("=")[1]
-                lines[i] = " ".join((key, "=",
-                                     replacements_dict.get(key.strip(), val)))
+                lines[i] = " ".join((key.rstrip(), "=",
+                                     replacements_dict.get(key.strip(),
+                                     val.lstrip())))
 
         return '\n'.join(lines) + '\n'
 
 if __name__ == "__main__":
 
-    driver_dir = "../../driver_files/"
-    met_dir = "../../met_data/plumber_met/"
-    output_dir = "outputs"
-    base_nml_fn = os.path.join(driver_dir, "cable.nml")
     cwd = os.getcwd()
-    nml_fn = os.path.join(cwd, "cable.nml")
+    met_dir = "../../met_data/plumber_met/"
+    log_dir = "logs"
+    output_dir = "outputs"
+    restart_dir = "restart_files"
+    aux_dir = "../../src/trunk/CABLE-AUX/"
+    cable_exe = "../../src/trunk/CABLE_trunk/offline/cable"
+
+    if not os.path.exists(restart_dir):
+        os.makedirs(restart_dir)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    base_nml_fn = os.path.join(aux_dir, "offline/cable.nml")
+    nml_fn = "cable.nml"
     shutil.copy(base_nml_fn, nml_fn)
-    veg_param_fn = "def_veg_params.txt"
-    log_dir = "log_files"
-    exe = "../../src/trunk/CABLE_trunk/offline/cable"
-    #exe = "../../src/tag-2.3.4/CABLE-2.3.4_tag/offline/cable"
-    aux_dir = "../../src/tag-2.3.4/CABLE-AUX/"
+
+    veg_fn = "def_veg_params_zr_clitt.txt"
+    soil_fn = "def_soil_params.txt"
+    base_veg_param_fn = os.path.join(aux_dir, veg_fn)
+    shutil.copy(base_nml_fn, nml_fn)
+    grid_fn = "gridinfo_CSIRO_1x1.nc"
     verbose = True
     all_met_files = False
 
@@ -145,6 +168,6 @@ if __name__ == "__main__":
         subset = ['TumbaFluxnet.1.4_met.nc']
         met_files = [os.path.join(met_dir, i) for i in subset]
 
-    C = RunCable(driver_dir, met_files, output_dir, nml_fn, veg_param_fn,
-                 log_dir, exe, aux_dir, verbose)
+    C = RunCable(met_files, output_dir, log_dir, aux_dir,
+                 nml_fn, veg_fn, soil_fn, grid_fn, cable_exe, verbose)
     C.main()
