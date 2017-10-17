8738c500a901acab0f235cd6a4e9c2cd012c789f
mdekauwe <mdekauwe@gmail.com>
2017-10-17 23:33:48 +1100

    Added sanity check

--- adjust_met_file.py
+++ adjust_met_file.py
@@ -123,9 +123,6 @@ class AdjustCableMetFile(object):
     def setup_ini_spin_met_file(self, start_yr_spin, end_yr_spin, start_met_yr,
                                 end_met_yr):
 
-        out = nc.Dataset('test.nc', 'w', format='NETCDF4')
-        ds = nc.Dataset(self.met_fname)
-
         total_yrs = end_yr_spin - start_yr_spin
 
         # Account for offset, first day stars 00:30 to 1.
@@ -138,96 +135,8 @@ class AdjustCableMetFile(object):
         end_idx = nc.num2date(ds.variables['time'][tindex1],
                               ds.variables['time'].units)
 
+        #var = ds.variables[v][tindex0:tindex1]
 
-
-        """
-        out.createDimension(dim, nc_fid.variables[dim].size)
-
-
-        print(time)
-        out.variables['time'][:] = time
-        #out.variables['lat'][:] = lats
-        #out.variables['lon'][:] = lons
-
-
-        #for ncattr in nc.variables['time'].ncattrs():
-        #    print ( w_nc_dim.setncattr(ncattr, nc.variables['time'].getncattr(ncattr)))
-        ds.close()
-        sys.exit()
-
-        vars = ds.variables.keys()
-        for v in vars:
-            var = ds.variables[v][tindex0:tindex1]
-            print(var)
-            print(ds.variables[v])
-            print("\n")
-            print(ds.variables[v].units)
-
-
-
-            sys.exit()
-        sys.exit()
-        #for attr in ds.ncattrs():
-        #    print(attr, '=', getattr(ds, attr))
-
-        #temp = rootgrp.createVariable("temp","f4",("time","level","lat","lon",))
-
-
-        #print(time[0], time[-1])
-        total_yrs = end_yr_spin - start_yr_spin
-
-        # Account for offset, first day stars 00:30 to 1.
-        start = dt.datetime(start_met_yr,1,1,0,30)
-        stop = dt.datetime(end_met_yr+1,1,1,0,0)
-        tindex0 = nc.date2index(start, ds.variables['time'], select='nearest')
-        tindex1 = nc.date2index(stop, ds.variables['time'], select='nearest')
-        start_idx = nc.num2date(ds.variables['time'][tindex0],
-                                ds.variables['time'].units)
-        end_idx = nc.num2date(ds.variables['time'][tindex1],
-                              ds.variables['time'].units)
-
-        vars = ds.variables.keys()
-        for v in vars:
-            print(v)
-            print(ds.variables[v])
-            print("\n")
-            print(ds.variables[v].units)
-
-            p = ds.variables[v][tindex0:tindex1]
-            print(p)
-            sys.exit()
-        sys.exit()
-
-        print(start)
-        print(stop)
-        print(tindex0)
-        print(tindex1)
-
-
-        print (nc.num2date(ds.variables['time'][tindex0], ds.variables['time'].units), nc.num2date(ds.variables['time'][tindex1], ds.variables['time'].units))
-
-        #print (nc.num2date(ds.variables['time'][tindex1], ds.variables['time'].units))
-
-
-
-        vars = ds.variables.keys()
-
-        print(vars)
-        time = nc.num2date(ds.variables['time'][:], ds.variables['time'].units)
-        print(time[0], time[-1])
-        sys.exit()
-
-
-        for v in ds.variables.keys():
-            var = ds.variables[v][tindex0:tindex1]
-            print(var)
-            print(ds.variables[v])
-            print("\n")
-            print(ds.variables[v].units)
-
-        for attr in dset.ncattrs():
-            print(attr, '=', getattr(ds, attr))
-        """
 
 
 
