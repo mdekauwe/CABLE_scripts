c5cdd4ceba1ba7b5c46497dfb836f85b123c124c
mdekauwe <mdekauwe@gmail.com>
2018-08-22 09:19:29 +1000

    Added more missing flags

--- run_cable_site_CNP.py
+++ run_cable_site_CNP.py
@@ -280,6 +280,8 @@ class RunCable(object):
                         "cable_user%limit_labile": ".F.",
                         "cable_user%SSNOW_POTEV": "'P-M'",
                         "cable_user%CASA_NREP": "0", # number of times to repeat CASA forcing
+                        "delsoilM":"0.01",
+                        "delsoilT":"0.1",
         }
         adjust_nml_file(self.nml_fname, replace_dict)
         sys.exit()
