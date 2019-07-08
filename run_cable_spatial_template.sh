ed7b95413aa40d448b4d29ff6521db8b864cccb2
mdekauwe <mdekauwe@gmail.com>
2019-07-08 21:13:07 +1000

    Changed mpi lib

--- run_cable_spatial_template.sh
+++ run_cable_spatial_template.sh
@@ -20,10 +20,10 @@ module load dot
 module add intel-cc
 module add intel-fc
 module load netcdf/4.3.3.1
-module load openmpi
+#module load openmpi
+module load intel-mpi
 module load subversion/1.9.0
 
-#module load intel-mpi/4.1.1.036
 
 #source activate nci
 activate nci
