519c9a79aef61428a8abf5f0939ed941ec598b78
mdekauwe <mdekauwe@gmail.com>
2019-07-24 16:00:30 +1000

    Added lon fix

--- generate_djf_nc_files.sh
+++ generate_djf_nc_files.sh
@@ -3,6 +3,10 @@
 
 cdo mergetime outputs/cable_out_*.nc outputs/all_yrs.nc
 
+# Fix the longitude issue in the CABLE output files...
+cdo sellonlatbox,-180,180,-90,90 outputs/all_yrs.nc outputs/all_yrs_fixed_long.nc
+mv outputs/all_yrs_fixed_long.nc outputs/all_yrs.nc
+
 #mm/s to mm/d
 #3 mean over 3 months (December to February)
 #11 skip the first 11 months (January to November)
