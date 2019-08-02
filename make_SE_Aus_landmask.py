4b1ed9bb5250bf8774b4f205ab1527d11f809517
mdekauwe <mdekauwe@gmail.com>
2019-08-02 15:00:25 +1000

    Added second level of screening

--- make_SE_Aus_landmask.py
+++ make_SE_Aus_landmask.py
@@ -14,17 +14,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-fname = "gswp3_landmask_nomissing.nc"
-out_fname = "SE_AUS_gswp3_landmask_nomissing.nc"
+fname = "/Users/mdekauwe/Desktop/gswp3_landmask_nomissing.nc"
+out_fname = "/Users/mdekauwe/Desktop/SE_AUS_gswp3_landmask_nomissing.nc"
+grid_fname = "/Users/mdekauwe/Desktop/gridinfo_mmy_MD_elev_orig_std_avg-sand_mask.nc"
 
 ds = xr.open_dataset(fname)
+ds_grid = xr.open_dataset(grid_fname)
+iveg = ds_grid.iveg.values
+
 
 # Subset by SE aus.
-ds['landsea'] = ds.landsea.where((ds.lon >= 140) & (ds.lon <= 154) & \
-                          (ds.lat >= -40) & (ds.lat <= -28), 1.0, 0.0)
-#plt.imshow(ds['landsea'])
-#plt.colorbar()
-#plt.show()
+ds['landsea'] = ds.landsea.where((ds.lon >= 140) & (ds.lon <= 154) &
+                                 (ds.lat >= -40) & (ds.lat <= -28),
+                                 1.0)
+
+# The logic above means we could re-introduce a masked sea pixel, so mask
+# where iveg is nan
+ds['landsea'] = ds.landsea.where(~np.isnan(iveg), 1.0)
 
 ds.to_netcdf(out_fname)
 ds.close()
