7186012820a9d9d43dbc272d88bac8ab34b04fa7
mdekauwe <mdekauwe@gmail.com>
2019-12-30 16:59:49 +1100

    Fixed ncores

--- run_pymc_wrapper.py
+++ run_pymc_wrapper.py
@@ -23,6 +23,7 @@ import matplotlib.pyplot as plt
 import netCDF4 as nc
 import uuid
 import pandas as pd
+import theano
 
 from cable_utils import adjust_nml_file
 from cable_utils import get_svn_info
@@ -35,6 +36,77 @@ def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+@theano.as_op([theano.tensor.dscalar, theano.tensor.dscalar], [theano.tensor.dvector])
+def run_and_unpack_cable(g1, vcmax):
+    params = np.array([g1, vcmax])
+    print(params)
+    param_names = ["g1", "vcmax"]
+
+    met_dir = "../../met_data/plumber_met"
+    log_dir = "logs"
+    output_dir = "outputs"
+    restart_dir = "restart_files"
+    namelist_dir = "namelists"
+    aux_dir = "../../src/CABLE-AUX/"
+    cable_src = "../../src/trunk/trunk"
+    mpi = False
+    num_cores = 1 # set to a number, if None it will use all cores...!
+    met_subset = ['TumbaFluxnet.1.4_met.nc']
+
+    # MCMC
+    adjust_params = True
+
+    print("\n")
+    print("===========")
+    print(params)
+    print("===========")
+    print("\n")
+
+    # ------------------------------------------- #
+    C = RunCable(met_dir=met_dir, log_dir=log_dir, output_dir=output_dir,
+                 restart_dir=restart_dir, aux_dir=aux_dir,
+                 namelist_dir=namelist_dir, met_subset=met_subset,
+                 cable_src=cable_src, mpi=mpi, num_cores=num_cores,
+                 adjust_params=adjust_params)
+
+
+
+    #temp = tempfile.NamedTemporaryFile(suffix=".nc")
+    #out_fname = temp.name
+    ##temp = tempfile.NamedTemporaryFile(suffix=".txt")
+    #out_log_fname = temp.name
+
+    id = str(uuid.uuid4())
+    out_fname = os.path.join(output_dir, "%s.nc" % (id))
+    out_log_fname = os.path.join(log_dir, "%s.txt" % (id))
+
+    C.main(param_names=param_names, param_values=params, out_fname=out_fname,
+           out_log_fname=out_log_fname)
+
+    f = nc.Dataset(out_fname)
+    time = nc.num2date(f.variables['time'][:],
+                       f.variables['time'].units)
+    df = pd.DataFrame(f.variables['Qle'][:,0,0], columns=['Qle'])
+    df['date'] = time
+    df = df.set_index('date')
+    df = df.between_time('5:00', '20:00')
+    #df = df[df.index.year == 2002]
+    df = df.resample("D").agg("mean")
+    mod = df.Qle.values
+    #plt.plot(mod)
+    #plt.show()
+    #sys.exit()
+    #ds = xr.open_dataset(out_fname, decode_times=False)
+    #mod = ds.Qle.values[:,0,0]
+    #print("model:", np.mean(mod), np.min(mod), np.max(mod))
+    if os.path.exists(out_fname):
+        os.remove(out_fname)
+    if os.path.exists(out_log_fname):
+        os.remove(out_log_fname)
+
+    return mod
 
 
 #------------- Setup paths...------------- #
@@ -84,48 +156,7 @@ with pm.Model() as model:
 
     # define likelihood, i.e. call CABLE...would be better as a func if we
     # can work that out
-
-    # unpack proposed values and call cable...
-    print("\n")
-    print("===========")
-    print(g1)
-    print(vcmax)
-    print("===========")
-    print("\n")
-    params = [g1, vcmax]
-    # ------------------------------------------- #
-    C = RunCable(met_dir=met_dir, log_dir=log_dir, output_dir=output_dir,
-                 restart_dir=restart_dir, aux_dir=aux_dir,
-                 namelist_dir=namelist_dir, met_subset=met_subset,
-                 cable_src=cable_src, mpi=mpi, num_cores=num_cores,
-                 adjust_params=adjust_params)
-
-    id = str(uuid.uuid4())
-    out_fname = os.path.join(output_dir, "%s.nc" % (id))
-    out_log_fname = os.path.join(log_dir, "%s.txt" % (id))
-
-    C.main(param_names=param_names, param_values=params, out_fname=out_fname,
-           out_log_fname=out_log_fname)
-
-    # Unpack model output...
-    f = nc.Dataset(out_fname)
-    time = nc.num2date(f.variables['time'][:],
-                       f.variables['time'].units)
-    df = pd.DataFrame(f.variables['Qle'][:,0,0], columns=['Qle'])
-    df['date'] = time
-    df = df.set_index('date')
-    df = df.between_time('5:00', '20:00')
-    #df = df[df.index.year == 2002]
-    df = df.resample("D").agg("mean")
-    mod = df.Qle.values
-
-    if os.path.exists(out_fname):
-        os.remove(out_fname)
-    if os.path.exists(out_log_fname):
-        os.remove(out_log_fname)
-
-
-
+    mod = pm.Deterministic('mod', run_and_unpack_cable(g1, vcmax))
     y_obs = pm.Normal('Y_obs', mu=mod, sd=sigma, observed=obs)
 
     # inference
