0b765ed4c8fb6afa1c2db193b35463ebddf25d08
mdekauwe <mdekauwe@gmail.com>
2020-01-03 17:47:33 +1100

    Added NUTs ver

---
+++ run_pymc_wrapper_NUTS.py
@@ -0,0 +1,386 @@
+#!/usr/bin/env python
+
+"""
+Wrapper script to do MCMC param estimation on CABLE. This is using PYMC3,
+everything needs to be cast to float 64
+
+THEANO_FLAGS='floatX=float64' ./run_pymc_wrapper.py
+
+That's all folks.
+"""
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (27.12.2019)"
+__email__ = "mdekauwe@gmail.com"
+
+
+import os
+import sys
+import glob
+import shutil
+import subprocess
+import multiprocessing as mp
+import numpy as np
+import pymc3 as pm
+import matplotlib.pyplot as plt
+import netCDF4 as nc
+import uuid
+import pandas as pd
+import theano
+import theano.tensor as tt
+
+from run_cable_site import RunCable
+
+#As we don't know the analytical form of our model we need a method to find
+#the gradient, we could use sympy? But below is the finite difference,
+#an iterative method with successively smaller interval sizes to check that
+#the gradient converges.
+#
+#https://docs.pymc.io/notebooks/blackbox_external_likelihood.html
+def gradients(vals, func, releps=1e-3, abseps=None, mineps=1e-9, reltol=1e-3,
+              epsscale=0.5):
+    """
+    Calculate the partial derivatives of a function at a set of values. The
+    derivatives are calculated using the central difference, using an iterative
+    method to check that the values converge as step size decreases.
+
+    Parameters
+    ----------
+    vals: array_like
+        A set of values, that are passed to a function, at which to calculate
+        the gradient of that function
+    func:
+        A function that takes in an array of values.
+    releps: float, array_like, 1e-3
+        The initial relative step size for calculating the derivative.
+    abseps: float, array_like, None
+        The initial absolute step size for calculating the derivative.
+        This overrides `releps` if set.
+        `releps` is set then that is used.
+    mineps: float, 1e-9
+        The minimum relative step size at which to stop iterations if no
+        convergence is achieved.
+    epsscale: float, 0.5
+        The factor by which releps if scaled in each iteration.
+
+    Returns
+    -------
+    grads: array_like
+        An array of gradients for each non-fixed value.
+    """
+
+    grads = np.zeros(len(vals))
+
+    # maximum number of times the gradient can change sign
+    flipflopmax = 10.
+
+    # set steps
+    if abseps is None:
+        if isinstance(releps, float):
+            eps = np.abs(vals)*releps
+            eps[eps == 0.] = releps  # if any values are zero set eps to releps
+            teps = releps*np.ones(len(vals))
+        elif isinstance(releps, (list, np.ndarray)):
+            if len(releps) != len(vals):
+                raise ValueError("Problem with input relative step sizes")
+            eps = np.multiply(np.abs(vals), releps)
+            eps[eps == 0.] = np.array(releps)[eps == 0.]
+            teps = releps
+        else:
+            raise RuntimeError("Relative step sizes are not a recognised type!")
+    else:
+        if isinstance(abseps, float):
+            eps = abseps*np.ones(len(vals))
+        elif isinstance(abseps, (list, np.ndarray)):
+            if len(abseps) != len(vals):
+                raise ValueError("Problem with input absolute step sizes")
+            eps = np.array(abseps)
+        else:
+            raise RuntimeError("Absolute step sizes are not a recognised type!")
+        teps = eps
+
+    # for each value in vals calculate the gradient
+    count = 0
+    for i in range(len(vals)):
+        # initial parameter diffs
+        leps = eps[i]
+        cureps = teps[i]
+
+        flipflop = 0
+
+        # get central finite difference
+        fvals = np.copy(vals)
+        bvals = np.copy(vals)
+
+        # central difference
+        fvals[i] += 0.5*leps  # change forwards distance to half eps
+        bvals[i] -= 0.5*leps  # change backwards distance to half eps
+        cdiff = (func(fvals)-func(bvals))/leps
+
+        while 1:
+            fvals[i] -= 0.5*leps  # remove old step
+            bvals[i] += 0.5*leps
+
+            # change the difference by a factor of two
+            cureps *= epsscale
+            if cureps < mineps or flipflop > flipflopmax:
+                # if no convergence set flat derivative (TODO: check if there is a better thing to do instead)
+                warnings.warn("Derivative calculation did not converge: setting flat derivative.")
+                grads[count] = 0.
+                break
+            leps *= epsscale
+
+            # central difference
+            fvals[i] += 0.5*leps  # change forwards distance to half eps
+            bvals[i] -= 0.5*leps  # change backwards distance to half eps
+            cdiffnew = (func(fvals)-func(bvals))/leps
+
+            if cdiffnew == cdiff:
+                grads[count] = cdiff
+                break
+
+            # check whether previous diff and current diff are the same within reltol
+            rat = (cdiff/cdiffnew)
+            if np.isfinite(rat) and rat > 0.:
+                # gradient has not changed sign
+                if np.abs(1.-rat) < reltol:
+                    grads[count] = cdiffnew
+                    break
+                else:
+                    cdiff = cdiffnew
+                    continue
+            else:
+                cdiff = cdiffnew
+                flipflop += 1
+                continue
+
+        count += 1
+
+    return grads
+
+# define a theano Op for our likelihood function
+class LogLikeWithGrad(tt.Op):
+
+    itypes = [tt.dvector] # expects a vector of parameter values when called
+    otypes = [tt.dscalar] # outputs a single scalar value (the log likelihood)
+
+    def __init__(self, loglike, data, x, sigma):
+        """
+        Initialise with various things that the function requires. Below
+        are the things that are needed in this particular example.
+
+        Parameters
+        ----------
+        loglike:
+            The log-likelihood (or whatever) function we've defined
+        data:
+            The "observed" data that our log-likelihood function takes in
+        x:
+            The dependent variable (aka 'x') that our model requires
+        sigma:
+            The noise standard deviation that out function requires.
+        """
+
+        # add inputs as class attributes
+        self.likelihood = loglike
+        self.data = data
+        self.x = x
+        self.sigma = sigma
+
+        # initialise the gradient Op (below)
+        self.logpgrad = LogLikeGrad(self.likelihood, self.data, self.sigma)
+
+    def perform(self, node, inputs, outputs):
+        # the method that is used when calling the Op
+        theta, = inputs  # this will contain my variables
+
+        # call the log-likelihood function
+        logl = self.likelihood(theta, self.x, self.data, self.sigma)
+
+        outputs[0][0] = np.array(logl) # output the log-likelihood
+
+    def grad(self, inputs, g):
+        # the method that calculates the gradients - it actually returns the
+        # vector-Jacobian product - g[0] is a vector of parameter values
+        theta, = inputs  # our parameters
+        return [g[0]*self.logpgrad(theta)]
+
+
+class LogLikeGrad(tt.Op):
+
+    """
+    This Op will be called with a vector of values and also return a vector of
+    values - the gradients in each dimension.
+    """
+    itypes = [tt.dvector]
+    otypes = [tt.dvector]
+
+    def __init__(self, loglike, data, sigma):
+        """
+        Initialise with various things that the function requires. Below
+        are the things that are needed in this particular example.
+
+        Parameters
+        ----------
+        loglike:
+            The log-likelihood (or whatever) function we've defined
+        data:
+            The "observed" data that our log-likelihood function takes in
+
+        sigma:
+            The noise standard deviation that out function requires.
+        """
+
+        # add inputs as class attributes
+        self.likelihood = loglike
+        self.data = data
+        self.sigma = sigma
+
+    def perform(self, node, inputs, outputs):
+        theta, = inputs
+
+        # define version of likelihood function to pass to derivative function
+        def lnlike(values):
+            return self.likelihood(values, self.data, self.sigma)
+
+        # calculate gradients
+        grads = gradients(theta, lnlike)
+
+        outputs[0][0] = grads
+
+def my_likelihood(theta, x, obs, sigma):
+    """
+    A Gaussian log-likelihood function for a model with parameters given
+    in theta
+    """
+
+    model = run_and_unpack_cable(theta)
+
+    return np.sum( -(0.5/sigma**2)*np.sum((obs - model)**2) )
+
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+def run_and_unpack_cable(theta):
+
+    g1, vcmax = theta  # unpack line gradient and y-intercept
+
+    params = np.array([g1, vcmax])
+    param_names = ["g1", "vcmax"]
+
+    met_dir = "../../met_data/plumber_met"
+    log_dir = "logs"
+    output_dir = "outputs"
+    restart_dir = "restart_files"
+    namelist_dir = "namelists"
+    aux_dir = "../../src/CABLE-AUX/"
+    cable_src = "../../src/trunk/trunk"
+    mpi = False
+    num_cores = 1 # set to a number, if None it will use all cores...!
+    met_subset = ['TumbaFluxnet.1.4_met.nc']
+
+    # MCMC
+    adjust_params = True
+
+    print("\n")
+    print("===========")
+    print(params)
+    print("===========")
+    print("\n")
+
+    # ------------------------------------------- #
+    C = RunCable(met_dir=met_dir, log_dir=log_dir, output_dir=output_dir,
+                 restart_dir=restart_dir, aux_dir=aux_dir,
+                 namelist_dir=namelist_dir, met_subset=met_subset,
+                 cable_src=cable_src, mpi=mpi, num_cores=num_cores,
+                 adjust_params=adjust_params)
+
+    #temp = tempfile.NamedTemporaryFile(suffix=".nc")
+    #out_fname = temp.name
+    ##temp = tempfile.NamedTemporaryFile(suffix=".txt")
+    #out_log_fname = temp.name
+
+    id = str(uuid.uuid4())
+    out_fname = os.path.join(output_dir, "%s.nc" % (id))
+    out_log_fname = os.path.join(log_dir, "%s.txt" % (id))
+    mcmc_tag = id
+    C.main(param_names=param_names, param_values=params,
+           out_fname=out_fname, out_log_fname=out_log_fname,
+           mcmc_tag=mcmc_tag)
+
+    f = nc.Dataset(out_fname)
+    time = nc.num2date(f.variables['time'][:],
+                       f.variables['time'].units)
+    df = pd.DataFrame(f.variables['Qle'][:,0,0], columns=['Qle'])
+    df['date'] = time
+    df = df.set_index('date')
+    df = df.between_time('5:00', '20:00')
+    #df = df[df.index.year == 2002]
+    df = df.resample("D").agg("mean")
+    mod = df.Qle.values
+    mod = mod.astype(np.float64)
+    #plt.plot(mod)
+    #plt.show()
+    #sys.exit()
+    #ds = xr.open_dataset(out_fname, decode_times=False)
+    #mod = ds.Qle.values[:,0,0]
+    #print("model:", np.mean(mod), np.min(mod), np.max(mod))
+
+
+    if os.path.exists(out_fname):
+        os.remove(out_fname)
+    if os.path.exists(out_log_fname):
+        os.remove(out_log_fname)
+
+    return mod
+
+
+
+# set up Observations...
+obs_dir = "../../flux_files/plumber"
+fn = os.path.join(obs_dir, 'TumbaFluxnet.1.4_flux.nc')
+f = nc.Dataset(fn)
+time = nc.num2date(f.variables['time'][:],
+                   f.variables['time'].units)
+df = pd.DataFrame(f.variables['Qle'][:,0,0], columns=['Qle'])
+df['date'] = time
+df = df.set_index('date')
+df = df.between_time('5:00', '20:00')
+#df = df[df.index.year == 2002]
+df = df.resample("D").agg("mean")
+obs = df.Qle.values
+obs = obs.astype(np.float64)
+#plt.plot(obs)
+#plt.show()
+
+#ds = xr.open_dataset(fn)
+#obs = ds.Qle.values[:,0,0]
+#uncert = np.sqrt(np.abs(obs))
+uncert = 0.1 * np.abs(obs) # not using, letting pymc fit this below...
+uncert = uncert.astype(np.float64)
+
+# create our Op
+logl = LogLikeWithGrad(my_likelihood, obs, uncert)
+
+#logl = LogLike(my_loglikelihood, obs, uncert)
+with pm.Model() as model:
+    g1 = pm.Uniform('g1', lower=0.0, upper=8.0)
+    vcmax = pm.Uniform('vcmax', lower=10.0, upper=120.0)
+
+    # convert to tensor vectors
+    theta = tt.as_tensor_variable([g1, vcmax])
+
+    # use a DensityDist (use a lamdba function to "call" the Op)
+    pm.DensityDist('likelihood', lambda v: logl(v), observed={'v': theta})
+
+    # Inference
+    step = pm.NUTS() # Hamiltonian MCMC with No U-Turn Sampler
+    #step = pm.Slice()
+    #step = pm.Metropolis()
+    trace = pm.sample(ndraws, tune=nburn, discard_tuned_samples=True)
+    
+_ = pm.traceplot(trace)
+pm.summary(trace)
