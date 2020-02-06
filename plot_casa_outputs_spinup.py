38e518e5032422e3b92f8cd62ee2570ef0d417c8
mdekauwe <mdekauwe@gmail.com>
2020-02-07 10:42:46 +1100

    Added spinup plot

--- plot_casa_outputs_spinup.py
+++ plot_casa_outputs_spinup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 """
-Plot some CASA outputs for the spinup
+CASA spinup diagnostics...C plant pools, C soil pools and CO2, Ndep and Pdep
 
 That's all folks.
 """
 __author__ = "Martin De Kauwe"
-__version__ = "1.0 (17.07.2018)"
+__version__ = "1.0 (07.02.2020)"
 __email__ = "mdekauwe@gmail.com"
 
 import matplotlib.pyplot as plt
@@ -19,82 +19,9 @@ import os
 import xarray as xr
 import glob
 
-
-def plot_carbon_fluxes(tag, cycle, ds, type, window=6):
+def plot_spinup_state(cycle, cf, cw, cr, ca, cs, cp, co2, ndep, pdep):
 
     fig = plt.figure(figsize=(15,6))
-    fig.subplots_adjust(hspace=0.3)
-    fig.subplots_adjust(wspace=0.3)
-    plt.rcParams['text.usetex'] = False
-    plt.rcParams['font.family'] = "sans-serif"
-    plt.rcParams['font.sans-serif'] = "Helvetica"
-    plt.rcParams['axes.labelsize'] = 12
-    plt.rcParams['font.size'] = 12
-    plt.rcParams['legend.fontsize'] = 12
-    plt.rcParams['xtick.labelsize'] = 12
-    plt.rcParams['ytick.labelsize'] = 12
-
-    ax1 = fig.add_subplot(2,3,1)
-    ax2 = fig.add_subplot(2,3,2)
-    ax3 = fig.add_subplot(2,3,3)
-    ax4 = fig.add_subplot(2,3,4)
-    ax5 = fig.add_subplot(2,3,5)
-    ax6 = fig.add_subplot(2,3,6)
-
-    ax1.set_title("GPP")
-    gpp = ds.Cgpp[:,0].to_dataframe()
-    ax1.plot(gpp.rolling(window=window).mean())
-    ax1.set_xticks(gpp.index.to_pydatetime())
-    ax1.locator_params(tight=True, nbins=6)
-
-    ax2.set_title("CUE")
-    ds['cue'] = ds.Cnpp[:,0]/ds.Cgpp[:,0]
-    cue = ds.cue.to_dataframe()
-    ax2.plot(cue.rolling(window=window).mean())
-    ax2.set_xticks(cue.index.to_pydatetime())
-    ax2.locator_params(tight=True, nbins=6)
-
-    ax3.set_title("LAI")
-    lai = ds.glai[:,0].to_dataframe()
-    ax3.plot(lai.rolling(window=window).mean())
-    ax3.set_xticks(lai.index.to_pydatetime())
-    ax3.locator_params(tight=True, nbins=6)
-
-    ax4.set_title("Vcmax")
-    vcmax = ds.vcmax[:,0].to_dataframe() * 1E6
-    ax4.plot(vcmax.rolling(window=window).mean())
-    ax4.set_xticks(vcmax.index.to_pydatetime())
-    ax4.locator_params(tight=True, nbins=6)
-
-    ax5.set_title("Allocation")
-    af = ds.fracCalloc[:,0].squeeze(dim=["land"], drop=True).to_dataframe()
-    aw = ds.fracCalloc[:,1].squeeze(dim=["land"], drop=True).to_dataframe()
-    ar = ds.fracCalloc[:,2].squeeze(dim=["land"], drop=True).to_dataframe()
-    ax5.plot(af.rolling(window=window).mean(), label="Af")
-    ax5.plot(aw.rolling(window=window).mean(), label="Aw")
-    ax5.plot(ar.rolling(window=window).mean(), label="Ar")
-    ax5.legend(numpoints=1, loc="best")
-    ax5.set_xticks(af.index.to_pydatetime())
-    ax5.locator_params(tight=True, nbins=6)
-
-    ax6.set_title("C labile")
-    clabile = ds.clabile[:,0].to_dataframe()
-    ax6.plot(clabile.rolling(window=window).mean())
-    ax6.set_xticks(clabile.index.to_pydatetime())
-    ax6.locator_params(tight=True, nbins=6)
-
-    plot_fname = "%s_%s_carbon_fluxes.pdf" % (tag, type)
-    plot_dir = "plots"
-    if not os.path.exists(plot_dir):
-        os.makedirs(plot_dir)
-
-    fig.savefig(os.path.join(plot_dir, plot_fname), bbox_inches='tight',
-                pad_inches=0.1)
-
-
-def plot_nitrogen_fluxes(tag, cycle, ds, type, window=6):
-
-    fig = plt.figure(figsize=(15,10))
     fig.subplots_adjust(hspace=0.3)
     fig.subplots_adjust(wspace=0.3)
     plt.rcParams['text.usetex'] = False
@@ -116,185 +43,102 @@ def plot_nitrogen_fluxes(tag, cycle, ds, type, window=6):
     ax8 = fig.add_subplot(3,3,8)
     ax9 = fig.add_subplot(3,3,9)
 
-    ax1.set_title("N fixation (g N/m^2/year)")
-    nfix = ds.Nminfix[:,0].to_dataframe()
-    ax1.plot(nfix.rolling(window=window).mean())
-    ax1.set_xticks(nfix.index.to_pydatetime())
-    ax1.locator_params(tight=True, nbins=6)
+    co2, ndep, pdep
 
-    ax2.set_title("N deposition (g N/m^2/year)")
-    ndep = ds.Nmindep[:,0].to_dataframe()
-    ax2.plot(ndep.rolling(window=window).mean())
-    ax2.set_xticks(ndep.index.to_pydatetime())
-    ax2.locator_params(tight=True, nbins=6)
+    ax1.set_title("C foliage")
+    ax1.plot(cf)
 
-    ax3.set_title("N loss")
-    nloss = ds.Nminloss[:,0].to_dataframe()
-    ax3.plot(nloss.rolling(window=window).mean())
-    ax3.set_xticks(nloss.index.to_pydatetime())
-    ax3.locator_params(tight=True, nbins=6)
+    ax2.set_title("C wood")
+    ax2.plot(cw)
 
-    ax4.set_title("N leach")
-    nleach = ds.Nminloss[:,0].to_dataframe()
-    ax4.plot(nleach.rolling(window=window).mean())
-    ax4.set_xticks(nleach.index.to_pydatetime())
-    ax4.locator_params(tight=True, nbins=6)
+    ax3.set_title("C root")
+    ax3.plot(cr)
 
-    ax5.set_title("N uptake")
-    nup = ds.Nupland[:,0].to_dataframe()
-    ax5.plot(nup.rolling(window=window).mean())
-    ax5.set_xticks(nup.index.to_pydatetime())
-    ax5.locator_params(tight=True, nbins=6)
+    ax4.set_title("C soil (active)")
+    ax4.plot(ca)
 
-    ax6.set_title("N gross mineralisation")
-    ngross = ds.Nsmin[:,0].to_dataframe()
-    ax6.plot(ngross.rolling(window=window).mean())
-    ax6.set_xticks(ngross.index.to_pydatetime())
-    ax6.locator_params(tight=True, nbins=6)
+    ax5.set_title("C soil (slow)")
+    ax5.plot(cs)
 
-    ax7.set_title("N net mineralisation")
-    nmin = ds.Nsnet[:,0].to_dataframe()
-    ax7.plot(nmin.rolling(window=window).mean())
-    ax7.set_xticks(nmin.index.to_pydatetime())
-    ax7.locator_params(tight=True, nbins=6)
+    ax6.set_title("C soil (passive)")
+    ax6.plot(cp)
 
-    ax8.set_title("N immobilisation")
-    nimmob = ds.Nsimm[:,0].to_dataframe()
-    ax8.plot(nimmob.rolling(window=window).mean())
-    ax8.set_xticks(nimmob.index.to_pydatetime())
-    ax8.locator_params(tight=True, nbins=6)
+    ax7.set_title("CO2")
+    ax7.plot(co2)
 
-    ax9.set_title("Leaf N:C")
-    ds['leaf_nc'] = ds.nplant[:,0,0]/ds.cplant[:,0,0]
-    leaf_nc = ds.leaf_nc.to_dataframe()
-    ax9.plot(leaf_nc.rolling(window=window).mean())
-    ax9.set_xticks(leaf_nc.index.to_pydatetime())
-    ax9.locator_params(tight=True, nbins=6)
+    ax8.set_title("N deposition")
+    ax8.plot(ndep)
 
-    plot_fname = "%s_%s_nitrogen_fluxes_spinup.pdf" % (tag, type)
+    ax9.set_title("P deposition")
+    ax9.plot(pdep)
+
+    plot_fname = "%s_spinup_carbon_pools.pdf" % (cycle)
     plot_dir = "plots"
     if not os.path.exists(plot_dir):
         os.makedirs(plot_dir)
 
-    fig.savefig(os.path.join(plot_dir, plot_fname), bbox_inches='tight',
-                pad_inches=0.1)
-
-def plot_phosphorus_fluxes(tag, cycle, ds, type, window=6):
-
-    fig = plt.figure(figsize=(15,10))
-    fig.subplots_adjust(hspace=0.3)
-    fig.subplots_adjust(wspace=0.3)
-    plt.rcParams['text.usetex'] = False
-    plt.rcParams['font.family'] = "sans-serif"
-    plt.rcParams['font.sans-serif'] = "Helvetica"
-    plt.rcParams['axes.labelsize'] = 12
-    plt.rcParams['font.size'] = 12
-    plt.rcParams['legend.fontsize'] = 12
-    plt.rcParams['xtick.labelsize'] = 12
-    plt.rcParams['ytick.labelsize'] = 12
-
-    ax1 = fig.add_subplot(3,3,1)
-    ax2 = fig.add_subplot(3,3,2)
-    ax3 = fig.add_subplot(3,3,3)
-    ax4 = fig.add_subplot(3,3,4)
-    ax5 = fig.add_subplot(3,3,5)
-    ax6 = fig.add_subplot(3,3,6)
-    ax7 = fig.add_subplot(3,3,7)
-    ax8 = fig.add_subplot(3,3,8)
-
-
-    ax1.set_title("P deposition (g N/m^2/year)")
-    pdep = ds.Pdep[:,0].to_dataframe()
-    ax1.plot(pdep.rolling(window=window).mean())
-    ax1.set_xticks(pdep.index.to_pydatetime())
-    ax1.locator_params(tight=True, nbins=6)
-
-    ax2.set_title("P loss")
-    ploss = ds.Ploss[:,0].to_dataframe()
-    ax2.plot(ploss.rolling(window=window).mean())
-    ax2.set_xticks(ploss.index.to_pydatetime())
-    ax2.locator_params(tight=True, nbins=6)
-
-    ax3.set_title("P leach")
-    pleach = ds.Pleach[:,0].to_dataframe()
-    ax3.plot(pleach.rolling(window=window).mean())
-    ax3.set_xticks(pleach.index.to_pydatetime())
-    ax3.locator_params(tight=True, nbins=6)
-
-    ax4.set_title("P uptake")
-    pup = ds.Pupland[:,0].to_dataframe()
-    ax4.plot(pup.rolling(window=window).mean())
-    ax4.set_xticks(pup.index.to_pydatetime())
-    ax4.locator_params(tight=True, nbins=6)
-
-    ax5.set_title("P gross mineralisation")
-    pgross = ds.Psmin[:,0].to_dataframe()
-    ax5.plot(pgross.rolling(window=window).mean())
-    ax5.set_xticks(pgross.index.to_pydatetime())
-    ax5.locator_params(tight=True, nbins=6)
-
-    ax6.set_title("P net mineralisation")
-    pnmin = ds.Psnet[:,0].to_dataframe()
-    ax6.plot(pnmin.rolling(window=window).mean())
-    ax6.set_xticks(pnmin.index.to_pydatetime())
-    ax6.locator_params(tight=True, nbins=6)
-
-    ax7.set_title("P immobilisation")
-    pimmob = ds.Psimm[:,0].to_dataframe()
-    ax7.plot(pimmob.rolling(window=window).mean())
-    ax7.set_xticks(pimmob.index.to_pydatetime())
-    ax7.locator_params(tight=True, nbins=6)
-
-    ax8.set_title("Leaf N:P")
-    ds['leaf_np'] = ds.nplant[:,0,0] / ds.cplant[:,0,0]
-    leaf_np = ds.leaf_np.to_dataframe()
-    ax8.plot(leaf_np.rolling(window=window).mean())
-    ax8.set_xticks(leaf_np.index.to_pydatetime())
-    ax8.locator_params(tight=True, nbins=6)
-
-    plot_fname = "%s_%s_phosphorus_fluxes.pdf" % (tag, type)
-    plot_dir = "plots"
-    if not os.path.exists(plot_dir):
-        os.makedirs(plot_dir)
-
+    fig.tight_layout()
     fig.savefig(os.path.join(plot_dir, plot_fname), bbox_inches='tight',
                 pad_inches=0.1)
 
 
-def open_file(fname):
+def get_data(casa_fname, cable_fname):
 
-    ds = xr.open_dataset(fname)
-    dates = pd.date_range('1/1/1850', periods=len(ds.time), freq='M')
-    ds['time'] = dates
+    ds = xr.open_dataset(casa_fname)
+    csoil = ds.csoil[-1,:,0]
+    cplant = ds.cplant[-1,:,0]
+    ndep = ds.Nmindep[-1,0]
+    pdep = ds.Pdep[-1,0]
 
-    return ds
+    ds = xr.open_dataset(cable_fname, decode_times=False)
+    co2 = ds.CO2air[-1,0,0]
 
+
+    return (cplant, csoil, co2, ndep, pdep)
 
 
 if __name__ == "__main__":
 
-    type = "spinup"
 
-    for cycle in ["C", "CN", "CNP"]:
+    site = "AU-Tum"
+    year = 1850
+
+    cf = []
+    cw = []
+    cr = []
+
+    ca = []
+    cs = []
+    cp = []
+
+    co2 = []
+    ndep = []
+    pdep = []
+
+    #for cycle in ["C", "CN", "CNP"]:
     #for cycle in ["C"]:
+    for cycle in ["C", "CN"]:
 
-        print(cycle)
+        files = glob.glob("outputs/%s_%s_out_casa_spin_*.nc" % (site, cycle))
+        nums = sorted([int(f.split(".")[0].split("_")[-1]) for f in files])
 
-        if cycle == "C":
-            experiment_id = "Cumberland_C"
-            tag = "C"
-        elif cycle == "CN":
-            experiment_id = "Cumberland_CN"
-            tag = "CN"
-        elif cycle == "CNP":
-            experiment_id = "Cumberland_CNP"
-            tag = "CNP"
+        for i in range(nums[0], nums[-1]):
+            print(cycle, i, nums[-1])
+            casa_fn = "outputs/%s_%s_out_casa_spin_%d.nc" % (site, cycle, i)
+            cable_fn = "outputs/%s_%s_out_cable_spin_%d.nc" % (site, cycle, i)
 
-        files = glob.glob("outputs/%s_out_CASA_ccp?.nc" % (experiment_id))
-        files.insert(0, "outputs/%s_out_CASA_zero.nc" % (experiment_id))
-        # open last file
-        ds = open_file(files[-1])
+            (cplant, csoil, co2x, ndepx, pdepx) = get_data(casa_fn, cable_fn)
 
-        plot_carbon_fluxes(tag, cycle, ds, type)
-        plot_nitrogen_fluxes(tag, cycle, ds, type)
-        plot_phosphorus_fluxes(tag, cycle, ds, type)
+            cf.append(cplant[0])
+            cw.append(cplant[1])
+            cr.append(cplant[2])
+
+            ca.append(csoil[0])
+            cs.append(csoil[1])
+            cp.append(csoil[2])
+
+            co2.append(co2x)
+            ndep.append(ndepx)
+            pdep.append(pdepx)
+
+        plot_spinup_state(cycle, cf, cw, cr, ca, cs, cp, co2, ndep, pdep)
